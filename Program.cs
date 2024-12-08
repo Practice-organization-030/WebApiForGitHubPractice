bbf3721d097c03dec77f93f5a6c33420451e37ad
muraliv030 <muraliv030@gmail.com>
2024-12-08 08:58:36 +0530

    Update Program.cs
    resolve merge conflicts

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle add some comments resolve conflicts
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
